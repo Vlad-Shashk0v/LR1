280cf9f1a25747a2ad604a9105bcba5b0401dc67
User <hallo935@mail.ru>
2023-05-27 18:04:52 +0300

    lr1 commit

---
+++ app/src/main/java/Shashkov/fit/bstu/by/MainActivity.kt
@@ -0,0 +1,43 @@
+package Shashkov.fit.bstu.by
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import Shashkov.fit.bstu.by.ui.theme.LR1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LR1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.errorContainer) {
+                    Greeting("Vlad")
+                }
+            }
+        }
+    }
+}
+//TODO test todo
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LR1Theme {
+        Greeting("Android")
+    }
+}
